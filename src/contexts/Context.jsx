3e5b0ba531395baa390deca1b1a7e749beb0765d
deborasous <deborapatriciadp27@gmail.com>
2023-04-14 15:46:41 -0300

    validando se o email e senha que o usuario digitou está armarmazenada, estejam incorretas, mostrar mensagem de erro

--- src/contexts/Context.jsx
+++ src/contexts/Context.jsx
@@ -8,24 +8,33 @@ export const UserContext = createContext();
 
 //instanciar validação
 const schema = yup.object().shape({
-  email: yup.string().email().required("Campo obrigatório"),
+  email: yup
+    .string()
+    .email("Digite um e-mail válido")
+    .required("Campo obrigatório")
+    .test("email-existe", "E-mail não cadastrado", (value) => {
+      const userEmail = "front.sous@gmail.com";
+      // const userEmail = localStorage.getItem("email");
+      return value === userEmail;
+    }),
   password: yup
     .string()
     .required("Campo obrigatório")
     .matches(
       /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
       "Senha deve conter no mínimo 8 caracteres e uma letra"
-    ),
+    )
+    .test("senha-correta", "Senha incorreta", (value) => {
+      const userPassword = "g25252525";
+      // const userPassword = localStorage.getItem("password");
+      return value === userPassword;
+    }),
 });
 
 //**Provider component */
 export const UserProvider = ({ children }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  //substituir por um banco de dados
-  const userEmail = "front.sous@gmail.com";
-  const userPassword = "g25252525";
 
   const {
     register,
@@ -37,6 +46,13 @@ export const UserProvider = ({ children }) => {
 
   const handleOnSubmit = (data) => {
     console.log(data.email, data.password);
+    //**remover depois que configurar o localStorage */
+    const userEmail = "front.sous@gmail.com";
+    const userPassword = "g25252525";
+
+    //trocar userEmail e userPassword depois de criar a página de cadastro de uruario
+    //localStorage.getItem("email");
+    // localStorage.getItem("password");
 
     if (data.email === userEmail && data.password === userPassword) {
       navigate("/");
